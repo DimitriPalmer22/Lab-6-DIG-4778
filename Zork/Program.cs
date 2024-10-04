22c85cfa15714d17c615db0301f178617d021327
DimitriPalmer22 <dimitriPalmer22@gmail.com>
2024-10-03 22:50:11 -0400

    Step 4

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -7,14 +7,20 @@ public class Program
         Console.WriteLine("Welcome to Zork!");
 
         // Get the command from the player
-        string input = Input();
+        var input = Input();
 
-        if (input == "quit")
-            Console.WriteLine("Thank you for playing!");
-        else if (input == "look")
-            Console.WriteLine("This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door.");
-        else
-            Console.WriteLine("Unrecognized command.");
+        // if (input == "quit")
+        //     Console.WriteLine("Thank you for playing!");
+        // else if (input == "look")
+        //     Console.WriteLine(
+        //         "This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door.");
+        // else
+        //     Console.WriteLine("Unrecognized command.");
+
+        // Convert the input to a command
+        var command = ToCommand(input);
+
+        Console.WriteLine($"Command: {command}");
     }
 
     /// <summary>
@@ -29,5 +35,31 @@ public class Program
         var input = Console.ReadLine().Trim().ToLower();
 
         return input;
+    }
+
+    /// <summary>
+    /// Convert the input to a command.
+    /// </summary>
+    /// <param name="input">A lowercase string</param>
+    /// <returns></returns>
+    private static Commands ToCommand(string input)
+    {
+        if (Enum.TryParse(input, true, out Commands cmd))
+            return cmd;
+
+        return Commands.UNKNOWN;
+    }
+
+    public enum Commands
+    {
+        QUIT,
+        LOOK,
+
+        NORTH,
+        SOUTH,
+        EAST,
+        WEST,
+
+        UNKNOWN
     }
 }
